40ec7536e8d66909405d0494b7d50f67d6900c5a
Nemuloso <hermes.ste@gmail.com>
2019-01-11 21:35:14 +0100

    Added content and structure

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -5,28 +5,79 @@ import { ThreeScene } from "../threescene/threescene";
 import { Footer } from "./footer";
 import { Section } from "./section";
 
-export class App extends React.Component {
+interface IAppProps {
+    stage: number;
+}
 
-    private stage: number = 0;
+export class App extends React.Component<IAppProps, any> {
+
     private sections: any[] = [
         {
             name: "Einleitung",
-            sectionTextDiv: <div>HAHA!</div>
-     }
+            sectionTextDiv:
+                <div>
+                    <div className="section_std section_huge">
+                        <div className="section_text">
+                            {"\n"
+                                + "Fachinformatiker für Anwendungsentwicklung\n"
+                                + "\n"
+                                + "Stefan Hermes\n"
+                                + "\n"
+                                + "VisCircle GmbH\n"
+                                + "Vahrenwalder Str. 207a\n"
+                                + "30165 Hannover"}
+                        </div>
+                    </div>
+                </div>
+        },
+        {
+            name: "Einleitung",
+            sectionTextDiv:
+                <div>
+                    <div className="section_std section_huge">
+                        <div className="section_text">
+                            {"\n"
+                                + "Autolack Shader\n"
+                                + "\n"
+                                + "Entwicklung eines Shader Programmes zur\n"
+                                + "Darstellung von Autolacken im Rahmen der\n"
+                                + "Abschlußprüfung der Ausbildung zum\n"
+                                + "Fachinformatiker\n"
+                                + "\n"}
+                        </div>
+                    </div>
+                </div>
+        },
     ];
 
     public constructor(props: any) {
         super(props);
+        this.state = {
+            stage: 0
+        };
+    }
+
+    private increaseStage(): void {
+        let state: any = this.state;
+
+        let newStage: number = state.stage + 1;
+        if (newStage === this.sections.length) {
+            newStage = 0;
+        }
+
+        this.setState({
+            stage: newStage
+        });
     }
 
     public render(): any {
         return (
             <div id="app_container" className="app">
-                <Header section={this.sections[this.stage].name}
-                onClick={() => console.log("lalala")} logo="../../assets/images/logo_V.png"></Header>
-                <Section textdiv={this.sections[this.stage].sectionTextDiv}></Section>
+                <Header section={this.sections[this.state.stage].name}
+                    onClick={() => this.increaseStage()} logo="../../assets/images/logo_V.png"></Header>
+                <Section textdiv={this.sections[this.state.stage].sectionTextDiv}></Section>
                 <ThreeScene></ThreeScene>
-                <Footer section={this.sections[this.stage].name} onClick={() => console.log("lalala")}></Footer>
+                <Footer section={this.sections[this.state.stage].name} onClick={() => alert("lalala")}></Footer>
             </div>
         );
     }
--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,22 +1,82 @@
 import * as React from "react";
 import "../../styles/app.styles.css";
 
-export function Footer(props: any): any {
-    return (
-        <div className="footer_container">
-            <hr />
-            <div className="footer" onClick={props.onClick}>
-                <div className="footer_textfield">
-                    <div>
-                        <span>
-                            Entwicklung eines Shader Programmes für Lackierte Oberflächen
-                        </span>
-                    </div>
-                    <div className="footer_section">
-                        {props.section}
-                    </div>
+interface IFooterProps {
+    onClick: () => void;
+    section: number;
+}
+
+interface IStageProps {
+    active: boolean;
+    name: string;
+}
+
+class Stage extends React.Component<IStageProps, any> {
+
+    public constructor(props: IStageProps) {
+        super(props);
+        this.state = {
+            active: props.active,
+            name: props.name
+        };
+    }
+
+    public render(): any {
+        let classString: string = "stage";
+        if (this.state.active) {
+            classString += " stage_active";
+        }
+        return (
+            <div className={classString}>
+                <span className="stage_span">{this.state.name}</span>
+            </div>
+        );
+    }
+}
+
+export class Footer extends React.Component<IFooterProps, any> {
+
+    private stages: IStageProps[] = [
+        { active: true, name: "Einleitung" },
+        { active: false, name: "Planung und Analyse" },
+        { active: false, name: "Thematische Einführung" },
+        { active: false, name: "Implementierung" },
+        { active: false, name: "Auswertung und Fazit" }
+    ];
+
+    public constructor(props: IFooterProps) {
+        super(props);
+        this.state = {
+            stage: 0,
+            onClick: props.onClick,
+            section: props.section
+        };
+    }
+
+    private renderStage(i: number): any {
+        return (
+            <Stage key={i} name={this.stages[i].name}
+                active={this.stages[i].active}
+            />);
+    }
+
+    private renderStages(): any[] {
+        let result: any[] = [this.renderStage(0)];
+        for (let i: number = 1; i < this.stages.length; i++) {
+            result.push(<div key={-i} className="line"></div>);
+            result.push(this.renderStage(i));
+        }
+        return result;
+    }
+
+    public render(): any {
+        return (
+            <div className="footer_container">
+                <hr />
+                <div className="footer" onClick={this.state.onClick}>
+                    {this.renderStages()}
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
